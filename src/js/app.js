7fe78db5e9ab12e6403655d5d63745273648162b
caio myra pereira <caiomyrapereira@hotmail.com>
2020-07-28 22:52:38 -0300

    Add update.

--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,5 @@
 console.log('initialize!')
 const doc = document;
 
-function myFunction() {
-    var x = document.getElementById("myLinks");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
-  }
-
 InitCarousel();
+
